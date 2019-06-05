bb97b90b318a6f6a39b211dfb7e8367303a2e9bf
hoang6k <daohoang.hust@gmail.com>
2019-06-05 10:36:18 +0700

    cholesky va lap jacobi

---
+++ cholesky.py
@@ -0,0 +1,51 @@
+import cmath
+import numpy as np
+
+A = [[1, 3, -2, 0, -2],
+     [3, 4, -5, 1, -3],
+     [-2, -5, 3, -2, 2],
+     [0, 1, -2, 5, 3],
+     [-2, -3, 2, 3, 4]]
+b = [[0.5], [5.4], [5.0], [7.5], [3.3]]
+
+#A = [[10, 2, -1, 2],
+#     [1, 5, 1, 0],
+#     [1, -2, -5, 1],
+#     [3, 0, 0, -9]]
+#b = [[-4], [-1], [2], [10]]
+
+def cholesky(A):
+    n = len(A)
+    A = np.asarray(A)
+    U = np.zeros((n,n), dtype=complex)
+    for i in range(n):
+        for j in range(n):
+            if i == j:
+                U[i,i] = cmath.sqrt(A[i,i] - np.sum(U[0:i,i]**2))
+            elif i < j:
+                U[i,j] = (A[i,j] - np.sum(U[0:i,i]*U[0:i,j])) / U[i,i]
+    return U, U.T
+
+def lower_tri(L, b):
+    n = len(L)
+    b = np.asarray(b)
+    y = np.zeros((n,1), dtype=complex)
+    for i in range(n):
+        y[i,0] = (b[i,0] - np.sum(L[i,0:i]*y[0:i,0])) / L[i,i]
+    return y
+
+def upper_tri(U, y):
+    n = len(U)
+    x = np.zeros((n,1), dtype=complex)
+    for i in range(n-1,-1,-1):
+        x[i,0] = (y[i,0] - np.sum(U[i,i+1:]*x[i+1:,0])) / U[i,i]
+    return x
+
+U, L = cholesky(A)
+y = lower_tri(L, b)
+x = upper_tri(U, y)
+x = np.asarray(x, dtype=float)
+n = len(x)
+print('He phuong trinh da cho co nghiem la:')
+for i in range(n):
+    print('x_{} = {}'.format(i+1, x[i,0]))
---
+++ lap_jacobi.py
@@ -0,0 +1,77 @@
+import numpy as np
+
+A = [[10, 2, -1, 2],
+     [1, 5, 1, 0],
+     [1, -2, -5, 1],
+     [3, 0, 0, -9]]
+b = [[-4], [-1], [2], [10]]
+
+def cheo_troi_hang(A, b, eps):
+    n = len(A)
+    I = np.identity(n)
+    A = np.asarray(A, dtype=float)
+    b = np.asarray(b, dtype=float)
+    for i in range(n):
+        b[i,0] = b[i,0] / A[i,i]
+        A[i,:] = A[i,:] / A[i,i]
+    alpha = I - A
+    beta = b
+#    print(alpha)
+#    print(beta)
+    x0 = np.zeros((n,1))
+    x1 = alpha.dot(x0) + beta
+    x = [x0, x1]
+    
+    # tinh chuan ma tran va so vong lap
+    q = np.around(np.linalg.norm(alpha), decimals=6) + 0.00001
+    k = np.around(np.log((1-q)*eps / np.linalg.norm(x1-x0)) / np.log(q)) + 1
+    print('norm = ' + str(q))
+    print('k = ' + str(int(k)))
+    
+    # thuc hien them k-1 vong lap
+    for i in range(int(k)-1):
+        x.append(alpha.dot(x[-1]) + beta)
+    return x[-1]
+
+def cheo_troi_cot(A, b, eps):
+    n = len(A)
+    I = np.identity(n)
+    A = np.asarray(A, dtype=float)
+    b = np.asarray(b, dtype=float)
+    
+    # luu lai mang doi bien
+    a_ii = np.asarray([A[i,i] for i in range(n)]).reshape((n,1))
+    for i in range(n):
+        A[:,i] = A[:,i] / A[i,i]
+    alpha = I - A
+    beta = b
+#    print(alpha)
+#    print(beta)
+    x0 = np.zeros((n,1))
+    x1 = alpha.dot(x0) + beta
+    x = [x0, x1]
+    
+    # tinh chuan ma tran va so vong lap
+    q = np.around(np.linalg.norm(alpha), decimals=6) + 0.00001
+    k = np.around(np.log((1-q)*eps / np.linalg.norm(x1-x0)) / np.log(q)) + 1
+    print('norm = ' + str(q))
+    print('k = ' + str(int(k)))
+    
+    # thuc hien them k-1 vong lap
+    for i in range(int(k)-1):
+        x.append(alpha.dot(x[-1]) + beta)
+    return x[-1] / a_ii
+
+eps = 1e-4
+
+X = cheo_troi_hang(A, b, eps)
+n = len(X)
+print('He phuong trinh da cho co nghiem giai voi cheo troi hang la:')
+for i in range(n):
+    print('x_{} = {}'.format(i+1, X[i]))
+
+X = cheo_troi_cot(A, b, eps)
+n = len(X)
+print('He phuong trinh da cho co nghiem giai voi cheo troi cot la:')
+for i in range(n):
+    print('x_{} = {}'.format(i+1, X[i]))
